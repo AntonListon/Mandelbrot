860fa6b6e2162c1e64488d62bea81ea443f5891b
AbrAlexis <92082385+AbrAlexis@users.noreply.github.com>
2023-11-17 20:52:55 +0100

    Grundlaget er lagt
    Programmet tager tre double inputs fra brugeren. Complex klassen er done

--- complex.java
+++ complex.java
@@ -2,14 +2,43 @@ public class Complex {
 
     private double re;
     private double im;
-
+    
     public Complex() {
-        re;
-        im;
+        this.re = 0;
+        this.im = 0;
     }
     
     public Complex(double re, double im) {
         this.re = re;
         this.im = im;
+    }
+
+    public Complex(Complex z) {
+
+    }
+
+    public double getRe() {
+        return this.re;
+    }
+
+    public double getIm() {
+        return this.im;
+    }
+
+    public double abs() {
+        return Math.sqrt(Math.pow(this.re, 2) + Math.pow(this.im, 2));
+    }
+
+    public Complex plus(Complex other) {
+
+        return new Complex(this.re + other.re, this.im + other.im);
+    }
+
+    public Complex times(Complex other) {
+        return new Complex((this.re * other.re) - (this.im * other.im), (this.im * other.re) + (this.re * other.im));
+    }
+
+    public String toString() {
+        return ""+this.re+" + "+this.im+"i";
     }
 }
